e51f11e310c0c2245b5d8658a4bf013e80222d1a
lynnemunini <muninilynne65@gmail.com>
2022-12-12 17:11:49 +0300

    Splash Screen

--- app/src/main/java/com/grayseal/forecastapp/screens/SplashScreen.kt
+++ app/src/main/java/com/grayseal/forecastapp/screens/SplashScreen.kt
@@ -1,12 +1,34 @@
 package com.grayseal.forecastapp.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import com.grayseal.forecastapp.R
+
 
 @Composable
-fun SplashScreen(navController: NavController){
+fun SplashScreen(navController: NavController) {
     Surface() {
-        
+        Column(
+            modifier = Modifier.fillMaxSize().background(colors.primary),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.sunny_cloudy),
+                contentDescription = "Splash Weather"
+            )
+        }
     }
 }
--- app/src/main/java/com/grayseal/forecastapp/ui/theme/Color.kt
+++ app/src/main/java/com/grayseal/forecastapp/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.grayseal.forecastapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val NavyBlue = Color(0xFF0b0b3d)
+val NavyBlue = Color(0xFF0a154b)
 val Blue = Color(0xFF1c85e7)
 val LightNavyBlue = Color(0xFF23224a)
 val White = Color(0xFFFFFFFF)
