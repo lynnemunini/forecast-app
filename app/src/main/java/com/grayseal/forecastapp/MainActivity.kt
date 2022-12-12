d337edb12df8503d86b1b87461ef11bc3f31e27c
lynnemunini <muninilynne65@gmail.com>
2022-12-12 16:02:47 +0300

    Set Up App UI

--- app/src/main/java/com/grayseal/forecastapp/MainActivity.kt
+++ app/src/main/java/com/grayseal/forecastapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.grayseal.forecastapp.navigation.WeatherNavigation
 import com.grayseal.forecastapp.ui.theme.ForecastApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,28 +20,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ForecastApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            WeatherApp {
+                WeatherNavigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WeatherApp(content: @Composable () -> Unit) {
+    ForecastApplicationTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            content()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ForecastApplicationTheme {
-        Greeting("Android")
+    WeatherApp{
+        WeatherNavigation()
     }
 }
