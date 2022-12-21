0b5b584afc65cf4a0001a11bffd88017f24a884e
lynnemunini <muninilynne65@gmail.com>
2022-12-21 17:44:04 +0300

    Set-Up base url and open weather map api key

---
+++ app/src/main/java/com/grayseal/forecastapp/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.grayseal.forecastapp.utils
+
+object Constants {
+    const val BASE_URL = "https://api.openweathermap.org/"
+    const val API_KEY = "f8b001a08778c78e5b8f87c14b36c269"
+}
