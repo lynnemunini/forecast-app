ba0f629c03f6bd2ad9f7498f363ed24689a832fc
lynnemunini <muninilynne65@gmail.com>
2023-01-03 14:55:57 +0300

    Disable ripple effect on NavBarItems

--- app/src/main/java/com/grayseal/forecastapp/widgets/NavigationBar.kt
+++ app/src/main/java/com/grayseal/forecastapp/widgets/NavigationBar.kt
@@ -1,5 +1,7 @@
 package com.grayseal.forecastapp.widgets
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
@@ -7,7 +9,13 @@ import androidx.compose.material.icons.outlined.Analytics
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.*
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,18 +30,38 @@ fun NavBar() {
         "Settings" to Icons.Outlined.Settings
     )
     val keys = listOf("Home", "Search", "Forecast", "Settings")
-    NavigationBar(
-        modifier = Modifier.fillMaxWidth(),
-        containerColor = Color.Transparent,
-        contentColor = Color.Transparent
+    CompositionLocalProvider(
+        LocalRippleTheme provides ClearRippleTheme
     ) {
-        keys.forEachIndexed { index, key ->
-            NavigationBarItem(
-                icon = { items[key]?.let { Icon(it, contentDescription = key) } },
-                selected = selectedItem == index,
-                onClick = { selectedItem = index },
-                colors = NavigationBarItemDefaults.colors(selectedIconColor = colors.secondary, unselectedIconColor = Color.White, indicatorColor = Color.Transparent)
-            )
+        NavigationBar(
+            modifier = Modifier.fillMaxWidth(),
+            containerColor = Color.Transparent,
+        ) {
+            keys.forEachIndexed { index, key ->
+                NavigationBarItem(
+                    icon = { items[key]?.let { Icon(it, contentDescription = key) } },
+                    selected = selectedItem == index,
+                    onClick = { selectedItem = index },
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = colors.secondary,
+                        unselectedIconColor = Color.White,
+                        indicatorColor = Color.Transparent
+                    )
+                )
+            }
         }
     }
+}
+
+object ClearRippleTheme : RippleTheme {
+    @Composable
+    override fun defaultColor(): Color = Color.Transparent
+
+    @Composable
+    override fun rippleAlpha() = RippleAlpha(
+        draggedAlpha = 0.0f,
+        focusedAlpha = 0.0f,
+        hoveredAlpha = 0.0f,
+        pressedAlpha = 0.0f,
+    )
 }
