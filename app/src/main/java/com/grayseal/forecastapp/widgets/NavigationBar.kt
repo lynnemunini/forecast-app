17c9ac1617e99544c9d0d133af966d1cebecaed9
lynnemunini <muninilynne65@gmail.com>
2023-01-03 16:06:09 +0300

    Remove ripple effect code

--- app/src/main/java/com/grayseal/forecastapp/widgets/NavigationBar.kt
+++ app/src/main/java/com/grayseal/forecastapp/widgets/NavigationBar.kt
@@ -1,67 +1,60 @@
 package com.grayseal.forecastapp.widgets
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Analytics
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material.ripple.LocalRippleTheme
-import androidx.compose.material.ripple.RippleAlpha
-import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.material.icons.filled.Analytics
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun NavBar() {
     var selectedItem by remember { mutableStateOf(0) }
     val items = mapOf(
-        "Home" to Icons.Outlined.Home,
-        "Search" to Icons.Outlined.Search,
-        "Forecast" to Icons.Outlined.Analytics,
-        "Settings" to Icons.Outlined.Settings
+        "Home" to Icons.Filled.Home,
+        "Search" to Icons.Filled.Search,
+        "Forecast" to Icons.Filled.Analytics,
+        "Settings" to Icons.Filled.Settings
     )
     val keys = listOf("Home", "Search", "Forecast", "Settings")
-    CompositionLocalProvider(
-        LocalRippleTheme provides ClearRippleTheme
+
+    NavigationBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+            .clip(
+                RoundedCornerShape(15.dp, 15.dp, 15.dp, 15.dp)
+            ),
+        containerColor = colors.primaryVariant,
     ) {
-        NavigationBar(
-            modifier = Modifier.fillMaxWidth(),
-            containerColor = Color.Transparent,
-        ) {
-            keys.forEachIndexed { index, key ->
-                NavigationBarItem(
-                    icon = { items[key]?.let { Icon(it, contentDescription = key) } },
-                    selected = selectedItem == index,
-                    onClick = { selectedItem = index },
-                    colors = NavigationBarItemDefaults.colors(
-                        selectedIconColor = colors.secondary,
-                        unselectedIconColor = Color.White,
-                        indicatorColor = Color.Transparent
-                    )
-                )
-            }
+        keys.forEachIndexed { index, key ->
+            NavigationBarItem(
+                icon = { items[key]?.let { Icon(it, contentDescription = key) } },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = colors.secondary,
+                    unselectedIconColor = Color.White,
+                    indicatorColor = colors.primaryVariant
+                ),
+                modifier = Modifier
+                    .size(50.dp)
+                    .padding(0.dp)
+            )
         }
     }
 }
 
-object ClearRippleTheme : RippleTheme {
-    @Composable
-    override fun defaultColor(): Color = Color.Transparent
-
-    @Composable
-    override fun rippleAlpha() = RippleAlpha(
-        draggedAlpha = 0.0f,
-        focusedAlpha = 0.0f,
-        hoveredAlpha = 0.0f,
-        pressedAlpha = 0.0f,
-    )
-}
